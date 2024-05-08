4904ea8829bda1de64907335020b027c2aaf9e52
jgarcia <jgarcia@106-03.info2>
2024-05-08 08:37:55 +0200

    Creado

---
+++ src/excepciones/Ejercicio01.java
@@ -0,0 +1,13 @@
+package excepciones;
+
+import java.util.Scanner;
+
+/**
+ * Clase que representa al Ejercicio01
+ */
+public class Ejercicio01 {
+
+	private int readInt(Scanner sc) {
+		return 0;
+	}
+}
